edc77db8253d74206371b0b882208fa13a85cf2d
Jude Rosario <judesrosario89@gmail.com>
2014-11-17 02:15:26 +0530

    Renamed file and fixed hook name

--- app/controller/class-ms-controller-invite-code.php
+++ app/controller/class-ms-controller-invite-code.php
@@ -1,11 +1,12 @@
 <?php
+
 class MS_Controller_Invite_Code extends MS_Controller {
 
 	public function __construct() {
 		parent::__construct();
 
 		$hook = 'protect-content_page_protected-content-invite-codes';
-		$this->add_action( 'load-' . $hook, 'admin_coupon_manager' );
+		$this->add_action( 'load-' . $hook, 'admin_invite_manager' );
 
 		$this->add_action( 'admin_print_scripts-' . $hook, 'enqueue_scripts' );
 		$this->add_action( 'admin_print_styles-' . $hook, 'enqueue_styles' );
@@ -14,7 +15,7 @@ class MS_Controller_Invite_Code extends MS_Controller {
 public function admin_invite_manager() {
 
 		/**
-		 * Save coupon add/edit
+		 * Save code add/edit
 		 */
 		$isset = array( 'submit', 'membership_id' );
 		if ( $this->validate_required( $isset, 'POST', false ) && $this->verify_nonce() && $this->is_admin_user() ) {
@@ -41,6 +42,31 @@ public function admin_invite_manager() {
 		}
 	}
 
+public function admin_invite_code() {
+		/**
+		 * Edit action view page request
+		 */
+		$isset = array( 'action', 'invite_id' );
+		if( $this->validate_required( $isset, 'GET', false ) && 'edit' == $_GET['action'] ) {
+			$invite_id = ! empty( $_GET['invite_id'] ) ? $_GET['invite_id'] : 0;
+			$data['invite_code'] = MS_Factory::load( 'MS_Model_Coupon', $invite_id );
+			$data['memberships'] = MS_Model_Membership::get_membership_names();
+			$data['memberships'][0] = __( 'Any', MS_TEXT_DOMAIN );
+			$data['action'] = $_GET['action'];
+
+			$view = MS_Factory::create( 'MS_View_Invite_Codes_Edit' );
+			$view->data = apply_filters( 'ms_view_invite_code_edit_data', $data );
+			$view->render();
+		}
+		/**
+		 * Coupon admin list page
+		 */
+		else {
+			$view = MS_Factory::create( 'MS_View_Invite_Codes_List' );
+			$view->render();
+		}
+	}
+	
 public function invite_code_do_action( $action, $invite_codes ) {
 		if( ! $this->is_admin_user() ) {
 			return;
