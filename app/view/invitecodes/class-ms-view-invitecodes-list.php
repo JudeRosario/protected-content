e50b6b92fad345d8ef91101ee6eca1d3fd457597
Jude Rosario <judesrosario89@gmail.com>
2014-11-17 04:25:58 +0530

    Added options to the Invite code list view

--- app/view/invitecodes/class-ms-view-invitecodes-list.php
+++ app/view/invitecodes/class-ms-view-invitecodes-list.php
@@ -1,30 +1,5 @@
 <?php
-/**
- * @copyright Incsub (http://incsub.com/)
- *
- * @license http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
- * MA 02110-1301 USA
- *
-*/
 
-/**
- * Renders Table of invite codes.
- *
- * Extends MS_View for rendering methods and magic methods.
- */
 
 class MS_View_Invite_Codes_List extends MS_View {
 
@@ -36,34 +11,67 @@ class MS_View_Invite_Codes_List extends MS_View {
 	 * @return string
 	 */
 	public function to_html() {
+		
 		$invitecodes_list = new MS_Helper_List_Table_Invite_Codes();
-		$invitecodes_list->prepare_items();
+		//$invitecodes_list->prepare_items();
 
 		$title = __( 'Invite Codes', MS_TEXT_DOMAIN );
-		$add_new_button = array(
+		$add_new_invite_codes_button = array(
 				'id' => 'add_new',
 				'type' => MS_Helper_Html::TYPE_HTML_LINK,
 				'url' => sprintf( 'admin.php?page=%s&action=edit&invite_id=0', MS_Controller_Plugin::MENU_SLUG . '-invitecodes' ),
 				'value' => __( 'Add New', MS_TEXT_DOMAIN ),
-				'class' => 'button',
-		);
+				'class' => 'button',);
 
+		$add_new_checkbox_signup = array(
+				'id' => 'signup-blocked',
+				'type' => MS_Helper_Html::INPUT_TYPE_RADIO_SLIDER,
+				'desc' => 'Block Sign ups for new users without Invite Codes. Existing uses will not be affected. Users can also be restricted by subscription model',
+				'value' => __( 'Add New', MS_TEXT_DOMAIN ),
+				'data_ms' => array(
+					'action' => MS_Controller_Settings::AJAX_ACTION_TOGGLE_SETTINGS,
+					'setting' => 'signup_blocked',
+				)
+				);
+		
+		$add_new_checkbox_show_at_login = array(
+				'id' => 'show-at-login',
+				'type' => MS_Helper_Html::INPUT_TYPE_RADIO_SLIDER,
+				'desc' => 'Show form at Sign up page',
+				'value' => __( 'Add New', MS_TEXT_DOMAIN ),
+				'data_ms' => array(
+					'action' => MS_Controller_Settings::AJAX_ACTION_TOGGLE_SETTINGS,
+					'setting' => 'show_at_login',
+				)
+				);
+		
 		ob_start();
 		?>
+		
 		<div class="wrap ms-wrap">
-			<?php
-				MS_Helper_Html::settings_header( array(
-					'title' => $title,
-					'title_icon_class' => 'ms-fa ms-fa-credit-card',
-				) );
-			?>
-			<div>
-				<?php MS_Helper_Html::html_element( $add_new_button );?>
-			</div>
+			<div class="invite-code-options">
+				<form id="main-options" action="" method="post">
+				<?php
+				MS_Helper_Html::settings_box(
+				array( $add_new_checkbox_signup),__( 'Block Sign-ups without Invite Codes', MS_TEXT_DOMAIN )
+				);
 
-			<form action="" method="post">
-				<?php $invitecodes_list->display(); ?>
-			</form>
+				MS_Helper_Html::settings_box(
+				array( $add_new_checkbox_show_at_login),__( 'Show form at log in page', MS_TEXT_DOMAIN )
+				);
+
+				MS_Helper_Html::settings_box(
+				array( $add_new_invite_codes_button),__( 'Create new Invite Codes', MS_TEXT_DOMAIN )
+				);
+				?>
+
+				</form>
+			</div>
+			<div  class="invite-code-tables">
+				<form id="table-options" action="" method="post">
+					<?php $invitecodes_list->display(); ?>
+				</form>
+			</div>
 		</div>
 
 		<?php
