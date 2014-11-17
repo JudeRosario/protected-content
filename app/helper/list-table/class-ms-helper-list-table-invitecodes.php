e6113d1becc8fb266d558af9e36ec6606f56e4fb
Blue Flames <mail.blueflames@gmail.com>
2014-11-18 02:51:44 +0530

    Helper Class updates for variable names

--- app/helper/list-table/class-ms-helper-list-table-invitecodes.php
+++ app/helper/list-table/class-ms-helper-list-table-invitecodes.php
@@ -2,7 +2,7 @@
 
 class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 
-	protected $id = 'code';
+	protected $id = 'invite_code';
 
 	public function __construct(){
 		parent::__construct(
@@ -16,29 +16,29 @@ class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 
 	public function get_columns() {
 		return apply_filters(
-			'membership_helper_list_table_invite_code_columns',
+			'invite_code_helper_list_table_invite_code_columns',
 			array(
-				'icb' => '<input type="checkbox" />',
+				'cb' => '<input type="checkbox" />',
 				'invite_code' => __( 'Invite Code', MS_TEXT_DOMAIN ),
-				'start_date' => __( 'Valid from', MS_TEXT_DOMAIN ),
-				'expiry_date' => __( 'Valid upto', MS_TEXT_DOMAIN ),
-				'membership_type' => __( 'Membership', MS_TEXT_DOMAIN ),
-				'times_used' => __( 'Used', MS_TEXT_DOMAIN ),
-				'max_uses' => __( 'Maximum uses', MS_TEXT_DOMAIN ),
+				'start_date' => __( 'Start date', MS_TEXT_DOMAIN ),
+				'expire_date' => __( 'Expire date', MS_TEXT_DOMAIN ),
+				'membership_type' => __( 'Subscription Type', MS_TEXT_DOMAIN ),
+				'used' => __( 'Times Used', MS_TEXT_DOMAIN ),
+				'remaining_uses' => __( 'Remaining uses', MS_TEXT_DOMAIN ),
 			)
 		);
 	}
 
 	public function get_hidden_columns() {
 		return apply_filters(
-			'membership_helper_list_table_membership_hidden_columns',
+			'invite_code_helper_list_table_hidden_columns',
 			array()
 		);
 	}
 
 	public function get_sortable_columns() {
 		return apply_filters(
-			'membership_helper_list_table_membership_sortable_columns',
+			'invite_code_helper_list_table_sortable_columns',
 			array()
 		);
 	}
@@ -51,7 +51,7 @@ class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 		);
 
 		$total_items = MS_Model_Invite_Code::get_invite_codes_count();
-		$per_page = $this->get_items_per_page( 'coupon_per_page', 10 );
+		$per_page = $this->get_items_per_page( 'invite_codes_per_page', 10 );
 		$current_page = $this->get_pagenum();
 
 		$args = array(
@@ -60,7 +60,7 @@ class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 		);
 
 		$this->items = apply_filters(
-			'membership_helper_list_table_coupon_items',
+			'invite_code_helper_list_table_invite_code_items',
 			MS_Model_Invite_Code::get_invite_codes( $args )
 		);
 
@@ -74,15 +74,15 @@ class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 
 	public function column_cb( $item ) {
 		return sprintf(
-			'<input type="checkbox" name="coupon_id[]" value="%1$s" />',
+			'<input type="checkbox" name="invite_id[]" value="%1$s" />',
 			esc_attr( $item->id )
 		);
 	}
 
-	public function column_code( $item ) {
+	public function column_invite_code( $item ) {
 		$actions = array();
 		$actions['edit'] = sprintf(
-			'<a href="?page=%s&action=%s&coupon_id=%s">%s</a>',
+			'<a href="?page=%s&action=%s&invite_id=%s">%s</a>',
 			esc_attr( $_REQUEST['page'] ),
 			'edit',
 			esc_attr( $item->id ),
@@ -92,7 +92,7 @@ class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 			'<span class="delete"><a href="%s">%s</a></span>',
 			wp_nonce_url(
 				sprintf(
-					'?page=%s&coupon_id=%s&action=%s',
+					'?page=%s&invite_id=%s&action=%s',
 					esc_attr( $_REQUEST['page'] ),
 					esc_attr( $item->id ),
 					'delete'
@@ -102,15 +102,15 @@ class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 			__( 'Delete', MS_TEXT_DOMAIN )
 		);
 
-		printf( '%1$s %2$s', $item->name, $this->row_actions( $actions ) );
+		printf( '%1$s %2$s', $item->invite_code, $this->row_actions( $actions) );
 	}
 
 	public function column_default( $item, $column_name ) {
 		$html = '';
 		switch ( $column_name ) {
-			case 'imembership':
+			case 'membership_type':
 				if ( MS_Model_Membership::is_valid_membership( $item->membership_type ) ) {
-					$membership = MS_Factory::load( 'MS_Model_Membership', $item->membership_type);
+					$membership = MS_Factory::load( 'MS_Model_Membership', $item->membership_type );
 					$html = $membership->name;
 				}
 				else {
@@ -118,9 +118,18 @@ class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 				}
 				break;
 
-			case 'iexpiry_date':
-				if ( $item->expiry_date ) {
-					$html = $item->expiry_date;
+			case 'start_date':
+				if ( $item->start_date) {
+					$html = $item->start_date;
+				}
+				else {
+					$html = __( 'Always Valid', MS_TEXT_DOMAIN );
+				}
+				break;
+
+			case 'expire_date':
+				if ( $item->expire_date ) {
+					$html = $item->expire_date;
 				}
 				else {
 					$html = __( 'No expiry', MS_TEXT_DOMAIN );
@@ -136,7 +145,7 @@ class MS_Helper_List_Table_Invite_Codes extends MS_Helper_List_Table {
 
 	public function get_bulk_actions() {
 		return apply_filters(
-			'membership_helper_list_table_membership_bulk_actions',
+			'invite_code_helper_list_table_invite_code_bulk_actions',
 			array(
 				'delete' => __( 'Delete', MS_TEXT_DOMAIN ),
 			)
