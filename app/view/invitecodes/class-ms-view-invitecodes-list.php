550e3011ff561a7aa45df8facd0a2a586f678e9f
Blue Flames <mail.blueflames@gmail.com>
2014-11-18 02:45:44 +0530

    Fixed URL in the Button

--- app/view/invitecodes/class-ms-view-invitecodes-list.php
+++ app/view/invitecodes/class-ms-view-invitecodes-list.php
@@ -3,23 +3,16 @@
 
 class MS_View_Invite_Codes_List extends MS_View {
 
-	/**
-	 * Create view output.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return string
-	 */
 	public function to_html() {
 		
-		// $invitecodes_list = new MS_Helper_List_Table_Invite_Codes();
-		// $invitecodes_list->prepare_items();
+		 $invitecodes_list = new MS_Helper_List_Table_Invite_Codes();
+		 $invitecodes_list->prepare_items();
 
 		$title = __( 'Invite Codes', MS_TEXT_DOMAIN );
 		$add_new_invite_codes_button = array(
 				'id' => 'add_new',
 				'type' => MS_Helper_Html::TYPE_HTML_LINK,
-				'url' => sprintf( 'admin.php?page=%s&action=edit&invite_id=0', MS_Controller_Plugin::MENU_SLUG . '-invitecodes' ),
+				'url' => sprintf( 'admin.php?page=%s&action=edit&invite_id=0', MS_Controller_Plugin::MENU_SLUG . '-invite-codes' ),
 				'value' => __( 'Add New', MS_TEXT_DOMAIN ),
 				'class' => 'button',);
 
